a9f793c99c46898a5b2653cdac35fc53c373c803
ricardo alarcon <geocodinglife@gmail.com>
2020-04-15 01:02:31 -0500

    error in root

--- template.rb
+++ template.rb
@@ -31,8 +31,57 @@ def copy_templates
   directory "app", force: true
 end
 
-def add_react_native
+# def add_root
+#   File.open('config/routes.rb', 'r+') do |file|
+#     lines = file.each_line.to_a
+#     lines[9] = "root 'home#index'"
+#     file.rewind
+#     file.write(lines.join)
+#   end
+# end
 
+def add_sidekiq
+  environment "config.active_job.queue_adapter = :sidekiq"
+
+  insert_into_file "config/routes.rb",
+    "require 'sidekiq/web'\n\n",
+    before: "Rails.application.routes.draw do"
+
+  content = <<-RUBY
+    authenticate :user, lambda { |u| u.admin? } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
+  RUBY
+  insert_into_file "config/routes.rb", "#{content}\n\n", after: "Rails.application.routes.draw do\n"
+  File.open('config/routes.rb', 'r+') do |file|
+    lines = file.each_line.to_a
+    lines[9] = "root 'home#index'"
+    file.rewind
+    file.write(lines.join)
+  end
+end
+
+def add_foreman
+  copy_file "Procfile"
+end
+
+def remove_comments
+  File.open('config/initializers/cors.rb', 'r+') do |file|
+    lines = file.each_line.to_a
+    lines[7][0]  = " "
+    lines[8][0]  = " "
+    lines[9][0]  = " "
+    lines[10][0] = " "
+    lines[11][0] = " "
+    lines[12][0] = " "
+    lines[13][0] = " "
+    lines[14][0] = " "
+    lines[15][0] = " "
+    file.rewind
+    file.write(lines.join)
+  end
+  # route "root to: 'home#index'"
+  insert_into_file 'Procfile', "web: cd #{app_name} && PORT=3000 yarn start"
 end
 
 def add_react
@@ -67,79 +116,83 @@ const Index = () => {
 export default Index;
    JS
 
-   content1 = <<-JS
-     import App from './views/App'
-   JS
+   insert_into_file "#{app_name}_web/src/views/home/index.js", "#{content}\n\n"
 
-   content2 = <<-JS
-     import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom'
-   JS
 
-  insert_into_file "#{app_name}_web/src/views/home/index.js", "#{content}\n\n"
-  insert_into_file "#{app_name}_web/src/index.js", "#{content1}"
-  insert_into_file "#{app_name}_web/src/views/App.jsx", "#{content2}"
+  content1 = <<-JS
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
+
+import '../stylesheets/App.css';
+import Index from './home/index';
+
+const Navigation = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink></li>
+      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/articles">Articles</NavLink></li>
+    </ul>
+  </nav>
+);
+
+class App extends Component {
+render() {
+  return (
+    <div className="App">
+      <Router>
+        <div className="container">
+          <Navigation />
+          <Main />
+        </div>
+      </Router>
+    </div>
+  );
+}
+}
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={Index} />
+  </Switch>
+);
+
+export default App;
+  JS
+
+  insert_into_file "#{app_name}_web/src/views/App.jsx", "#{content1}\n\n"
+
+# const fs = require('fs')
+# const someFile = #{app_name}_web/src/views/App.jsx
+# fs.readFile(someFile, 'utf8', function (err, data) {
+#   if (err) {
+#     return console.log(err);
+#   }
+#   let result = data.split('\n')
+#   result[3] = "import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom'"
+#   result1 = result.join("\n")
+#   fs.writeFile(someFile, result1, 'utf8', function (err) {
+#      if (err) return console.log(err);
+#   });
+# });
+
+
+`
+   const fs = require('fs')
+   const someFile = #{app_name}_web/src/index.js
+   fs.readFile(someFile, 'utf8', function (err, data) {
+     if (err) {
+       return console.log(err);
+     }
+     let result = data.replace('./App', './views/App');
+
+     fs.writeFile(someFile, result, 'utf8', function (err) {
+        if (err) return console.log(err);
+     });
+   });
+`
 
   # `node flash.js #{app_name}.web`
 end
-
-def add_sidekiq
-  environment "config.active_job.queue_adapter = :sidekiq"
-
-  insert_into_file "config/routes.rb",
-    "require 'sidekiq/web'\n\n",
-    before: "Rails.application.routes.draw do"
-
-  content = <<-RUBY
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
-  RUBY
-  insert_into_file "config/routes.rb", "#{content}\n\n", after: "Rails.application.routes.draw do\n"
-end
-
-def add_foreman
-  copy_file "Procfile"
-end
-
-def add_root
-  File.open('config/initializers/routes.rb', 'r+') do |file|
-    file.each_line.to_a
-    file[10] = root to: 'home#index'
-    file.rewind
-    file.write(lines.join)
-  end
-end
-
-def remove_comments
-  File.open('config/initializers/cors.rb', 'r+') do |file|
-  lines = file.each_line.to_a
-  lines[7][0]  = ""
-  lines[8][0]  = ""
-  lines[9][0]  = ""
-  lines[10][0] = ""
-  lines[11][0] = ""
-  lines[12][0] = ""
-  lines[13][0] = ""
-  lines[14][0] = ""
-  lines[15][0] = ""
-  file.rewind
-  file.write(lines.join)
-end
-
-#   cors = <<-RUBY
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'http://localhost:3000'
-#     resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
-#   end
-# end
-# RUBY
-
-  # insert_into_file 'config/initializers/cors.rb', "#{cors}\n\n\n\n"
-  # route "root to: 'home#index'"
-  insert_into_file 'Procfile', "web: cd #{app_name} && PORT=3000 yarn start"
-end
-
 
 source_paths
 
@@ -147,13 +200,14 @@ add_gems
 
 after_bundle do
   add_users
+  # add_root
   add_sidekiq
-  add_react
   add_foreman
   copy_templates
-  remove_comments
   rails_command "db:create"
   rails_command "db:migrate"
+  remove_comments
+  add_react
 
   git :init
   git add: "."
