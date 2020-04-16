63970acd4e1367dcb0b08b3ab04b27889ee040b0
ricardo alarcon <geocodinglife@gmail.com>
2020-04-15 22:03:38 -0500

    is working

--- template.rb
+++ template.rb
@@ -9,6 +9,7 @@ def add_gems
   gem 'pry-rails', '~> 0.3.9'
   gem 'awesome_print', '~> 1.8'
   gem 'faker', '~> 2.11'
+  gem 'rack-cors'
 end
 
 generate "controller", "home index"
@@ -18,7 +19,6 @@ def add_users
   environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }",
               env: 'development'
 
-  # route "root to: 'home#index'"
   generate :devise, "User", "username", "name", "admin:boolean"
 
   in_root do
@@ -30,15 +30,6 @@ end
 def copy_templates
   directory "app", force: true
 end
-
-# def add_root
-#   File.open('config/routes.rb', 'r+') do |file|
-#     lines = file.each_line.to_a
-#     lines[9] = "root 'home#index'"
-#     file.rewind
-#     file.write(lines.join)
-#   end
-# end
 
 def add_sidekiq
   environment "config.active_job.queue_adapter = :sidekiq"
@@ -53,12 +44,14 @@ def add_sidekiq
     end
   RUBY
   insert_into_file "config/routes.rb", "#{content}\n\n", after: "Rails.application.routes.draw do\n"
+
   File.open('config/routes.rb', 'r+') do |file|
     lines = file.each_line.to_a
-    lines[9] = "root 'home#index'"
+    lines[9] = "  root 'home#index'"
     file.rewind
     file.write(lines.join)
   end
+
 end
 
 def add_foreman
@@ -70,7 +63,7 @@ def remove_comments
     lines = file.each_line.to_a
     lines[7][0]  = " "
     lines[8][0]  = " "
-    lines[9][0]  = " "
+    lines[9][0]  = "      origins 'http://localhost:3000'"
     lines[10][0] = " "
     lines[11][0] = " "
     lines[12][0] = " "
@@ -80,8 +73,8 @@ def remove_comments
     file.rewind
     file.write(lines.join)
   end
-  # route "root to: 'home#index'"
-  insert_into_file 'Procfile', "web: cd #{app_name} && PORT=3000 yarn start"
+
+  insert_into_file 'Procfile', "web: cd #{app_name}_web && PORT=3000 yarn start"
 end
 
 def add_react
@@ -94,11 +87,13 @@ def add_react
    mkdir src/stylesheets
    mkdir src/views
    mkdir src/views/home
-   touch src/views/home/index.js
+   touch src/views/home/Index.js
    mv src/logo.svg src/images/logo.svg
-   mv src/index.css src/stylesheets/index.css
+   rm -R src/index.css
+   touch src/stylesheets/index.css
    mv src/App.css src/stylesheets/App.css
-   mv src/App.js src/views/App.jsx
+   rm -R src/App.js
+   touch src/views/App.js
    mv src/App.test.js src/views/App.test.js
   `
 
@@ -116,64 +111,50 @@ const Index = () => {
 export default Index;
    JS
 
-   insert_into_file "#{app_name}_web/src/views/home/index.js", "#{content}\n\n"
-
+  insert_into_file "#{app_name}_web/src/views/home/Index.js", "#{content}\n\n"
 
   content1 = <<-JS
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
+  import React, { Component } from 'react';
+  import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
 
-import '../stylesheets/App.css';
-import Index from './home/index';
+  import Index from './home/Index';
+  import logo from '../images/logo.svg';
+  import '../stylesheets/App.css';
 
-const Navigation = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink></li>
-      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/articles">Articles</NavLink></li>
-    </ul>
-  </nav>
-);
-
-class App extends Component {
-render() {
-  return (
-    <div className="App">
-      <Router>
-        <div className="container">
-          <Navigation />
-          <Main />
-        </div>
-      </Router>
-    </div>
+  const Navigation = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink></li>
+        <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/articles">Articles</NavLink></li>
+      </ul>
+    </nav>
   );
-}
-}
 
-const Main = () => (
-  <Switch>
-    <Route exact path="/" component={Index} />
-  </Switch>
-);
+  class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <Router>
+            <div className="container">
+              <Navigation />
+              <Main />
+            </div>
+          </Router>
+        </div>
+      );
+    }
+  }
 
-export default App;
+  const Main = () => (
+    <Switch>
+      <Route exact path="/" component={Index} />
+    </Switch>
+  );
+
+  export default App;
   JS
 
-  insert_into_file "#{app_name}_web/src/views/App.jsx", "#{content1}\n\n"
-
-# const fs = require('fs')
-# const someFile = #{app_name}_web/src/views/App.jsx
-# fs.readFile(someFile, 'utf8', function (err, data) {
-#   if (err) {
-#     return console.log(err);
-#   }
-#   let result = data.split('\n')
-#   result[3] = "import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom'"
-#   result1 = result.join("\n")
-#   fs.writeFile(someFile, result1, 'utf8', function (err) {
-#      if (err) return console.log(err);
-#   });
-# });
+  insert_into_file "#{app_name}_web/src/views/App.js", "#{content1}\n\n"
 
 
 `
@@ -184,8 +165,13 @@ export default App;
        return console.log(err);
      }
      let result = data.replace('./App', './views/App');
+     let result1 = data.replace('./index.css', './stylesheets/index.css');
 
      fs.writeFile(someFile, result, 'utf8', function (err) {
+        if (err) return console.log(err);
+     });
+
+     fs.writeFile(someFile, result1, 'utf8', function (err) {
         if (err) return console.log(err);
      });
    });
@@ -194,13 +180,13 @@ export default App;
   # `node flash.js #{app_name}.web`
 end
 
+
 source_paths
 
 add_gems
 
 after_bundle do
   add_users
-  # add_root
   add_sidekiq
   add_foreman
   copy_templates
