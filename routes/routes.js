f84afed096e276ba8e3122393325f4e66ec6cc75
geocodinglife <geocodinglife@gmail.com>
2021-04-03 19:14:25 -0500

    Add routes

---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Home from '../views/Home';
+import logo from '../images/logo.svg';
+import '../stylesheets/App.css';
+
+
+const ROUTES = [
+  {
+    component: Home,
+    path: '/',
+  }
+];
+
+export default ROUTES;
